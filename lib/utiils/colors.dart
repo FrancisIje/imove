e0207309dc645436ec7e3ddadaf842d1ae7954fd
FrancisIje <ijenebefrancis@gmail.com>
2024-11-26 04:53:48 +0100

    fix(color)

--- lib/utiils/colors.dart
+++ lib/utiils/colors.dart
@@ -2,7 +2,7 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const backgroundColor = Color(0xFFE5E5E5);
+  static const backgroundColor = Colors.white;
   static const textFieldBackgroundColor = Color(0xFFF0F5F5);
   static const primaryColor = Color(0xFF9a0000);
   static FlexSchemeColor scheme = FlexSchemeColor.from(
