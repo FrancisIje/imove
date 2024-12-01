9661a289e3b62e83ae3d180bc31a44b0f0329052
FrancisIje <ijenebefrancis@gmail.com>
2024-12-01 17:46:52 +0100

    chore(utils):added imports

--- lib/utiils/utils.dart
+++ lib/utiils/utils.dart
@@ -1,4 +1,13 @@
 library utils;
 
+import 'package:go_router/go_router.dart';
+import 'package:imove/views/home_screen.dart';
+import 'package:imove/views/login.dart';
+import 'package:imove/views/onboarding.dart';
+import 'package:imove/views/splash_screen.dart';
+import 'package:imove/views/verification.dart';
+
 export './colors.dart';
 export './textstyle.dart';
+
+part './routes.dart';
