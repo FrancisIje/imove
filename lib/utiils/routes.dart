0244c3d73d5963368388b13ef0a4bfda86f18c13
FrancisIje <ijenebefrancis@gmail.com>
2024-12-01 17:17:43 +0100

    chore(routes):added routes

---
+++ lib/utiils/routes.dart
@@ -0,0 +1,48 @@
+part of './utils.dart';
+
+final router = GoRouter(
+  initialLocation: '/splash',
+  redirect: (context, state) {
+    // final isLoggedIn = AuthService.emailPassword().isUserLoggedIn;
+    // if (state.path != "/splash" && state.path != null && isLoggedIn == false) {
+    //   return "/login";
+    // }
+
+    return null;
+  },
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/onboarding',
+      builder: (context, state) => const OnboardingScreen(),
+    ),
+    GoRoute(
+        path: '/register',
+        builder: (context, state) => const OnboardingScreen(),
+        routes: [
+          GoRoute(
+            path: '/verify',
+            builder: (context, state) => const VerificationScreen(),
+          ),
+        ]),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    //  GoRoute(
+    //   path: '/instant-delivery',
+    //   builder: (context, state) => const (),
+    // ),
+    //  GoRoute(
+    //   path: '/schedule-delivery',
+    //   builder: (context, state) => const (),
+    // ),
+  ],
+);
