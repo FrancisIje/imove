74491a8cc0e2b81af9c4b2808377b090f67f2120
FrancisIje <ijenebefrancis@gmail.com>
2024-12-25 16:25:13 +0100

    added routes imports

--- lib/utiils/utils.dart
+++ lib/utiils/utils.dart
@@ -1,7 +1,12 @@
 library utils;
 
 import 'package:go_router/go_router.dart';
+import 'package:imove/views/add_details.dart';
+import 'package:imove/views/confirm_details.dart';
+import 'package:imove/views/courier_screen.dart';
+import 'package:imove/views/delivery_details.dart';
 import 'package:imove/views/home.dart';
+import 'package:imove/views/instant_delievry.dart';
 import 'package:imove/views/login.dart';
 import 'package:imove/views/onboarding.dart';
 import 'package:imove/views/signup.dart';
